648d219e6b0c088d71ab3eecc8277d3ca722c963
Shivani Ghode <shivanighode8@gmail.com>
2026-02-17 13:36:51 +0530

    Updated modified files

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
+gsap.registerPlugin(ScrollTrigger);
+
 let tl = gsap.timeline();
 
-let homepageAnimation = function(){
-tl.from("nav", {
+function homepageAnimation() {
+  tl.from("nav", {
     y: 50,
     opacity: 0,
-    delay: 0.5
-});
+    delay: 0.5,
+  });
 
-tl.from(".middle h1", {
+  tl.from(".middle h1", {
     xPercent: -20,
     opacity: 0,
-    stagger: 0.2
-});
+    stagger: 0.2,
+  });
 
-tl.from(".middle p", {
+  tl.from(".middle p", {
     xPercent: -20,
     opacity: 0,
-    stagger: 0.2
-});
+    stagger: 0.2,
+  });
 
-tl.from(".search", {
+  tl.from(".search", {
     yPercent: 100,
-    opacity: 0
-});
+    opacity: 0,
+  });
 }
+
 homepageAnimation();
 
-//animation for category
-let categoryPageAnimation = function(){
-    gsap.from(" .category categoryHead",{
-       y:100,
-       opacity:0,
-       duration:0.5,
-       scrollTrigger:{
-        trigger:".category",
-        scroller:"body",
-        start: "top 25%",
-       }
-    })
+// Category animation
+function categoryPageAnimation() {
+  gsap.from(".category .categoryHead", {
+    y: 100,
+    opacity: 0,
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: ".category",
+      start: "top 25%",
+    },
+  });
 
-    gsap.from(".category .card",{
- y:150,
- opacity:0,
- duration:0.5,
- delay:0.5,
- stagger:0.2,
-scrollTrigger:{
-        trigger:".category",
-        scroller:"body",
-        start: "top 25%",
-    }
-    
-})
+  gsap.from(".category .card", {
+    y: 150,
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.5,
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".category",
+      start: "top 25%",
+    },
+  });
 }
 
 categoryPageAnimation();
 
-//Code of page3 -- Swiper js
-
-let swiper =  new Swiper(".swiper",{
-     slidesPerView : 4,
-    autoplay:true,
-    navigation:{
-        nextEl: ".next",
-        prevEl : ".pre"
-    }
-})
+// Swiper
+let swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  autoplay: true,
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+});
